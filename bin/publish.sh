feb570c6dc0c5ddea7d148d582a068793b5aa77b
Ashish Agarwal <agarwal1975@gmail.com>
2014-05-26 18:49:06 -0400

    implement script to publish site

---
+++ bin/publish.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+omake site-clean
+omake PRODUCTION=true
+
+find _build/site -type d -exec chmod 755 {} \;
+find _build/site -type f -exec chmod 644 {} \;
+
+rsync -av --exclude .DS_Store --delete --delete-excluded \
+  _build/site/* \
+  cufp:~/cufp.org/
+
+# Make sure you don't accidentally keep working with PRODUCTION files.
+omake site-clean
