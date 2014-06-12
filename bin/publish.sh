0e77ead2b43a87b5c4e977815d834ecdd2d42ade
Ashish Agarwal <agarwal1975@gmail.com>
2014-06-12 14:41:30 -0400

    publish.sh: delete deleted files
    Previous version wouldn’t delete deleted files in remote if the file
    was in the site root.

--- bin/publish.sh
+++ bin/publish.sh
@@ -7,7 +7,7 @@ find _build/site -type d -exec chmod 755 {} \;
 find _build/site -type f -exec chmod 644 {} \;
 
 rsync -av --exclude .DS_Store --delete --delete-excluded \
-  _build/site/* \
+  _build/site/ \
   cufp:~/cufp.org/
 
 # Make sure you don't accidentally keep working with PRODUCTION files.
